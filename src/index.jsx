9cef4b481a46d1b0e0940ac82ce97b04227b277d
kcirekcom <erick.f.mock@gmail.com>
2018-09-22 15:52:39 -0700

    Re-updated index.jsx file to handle react routing

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,23 @@
 
 import * as React from 'react';
 import { render } from 'react-dom';
-// import { createBrowserHistory } from 'history';
-// import HomePage from './components/HomePage.jsx';
-// import Visualization from './components/Visualization.jsx';
-// import Prototype from './components/Prototype.jsx';
-// import Development from './components/Development.jsx';
-// import { Switch, Route } from 'react-router';
+import { createBrowserHistory } from 'history';
+import HomePage from './components/HomePage.jsx';
+import Visualization from './components/Visualization.jsx';
+import Prototype from './components/Prototype.jsx';
+import Development from './components/Development.jsx';
+import { Switch, Route } from 'react-router';
 
 import './scss/main.scss';
 
-// const browserHistory = createBrowserHistory();
+const browserHistory = createBrowserHistory();
 
 render(
-  <div>Hello</div>,
+  <Switch history={browserHistory}>
+    <Route exact path='/' component={HomePage}/>
+    <Route path='/visualizations' component={Visualization}/>
+    <Route path='/prototypes' component={Prototype}/>
+    <Route path='/development' component={Development}/>
+  </Switch>,
   document.getElementById('react-src')
 );
