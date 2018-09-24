5aaf57fc2b9e4ef335446034e6237a43726e805b
kcirekcom <erick.f.mock@gmail.com>
2018-09-23 18:33:45 -0700

    Installed react-router-dom dependency to fix Link component warnings Added assets and data for project Built out Navbar and HomePage components Restructured react router in index.jsx file to house Navbar component

--- src/index.jsx
+++ src/index.jsx
@@ -3,22 +3,26 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { createBrowserHistory } from 'history';
+import Navbar from './components/Navbar.jsx';
 import HomePage from './components/HomePage.jsx';
 import Visualization from './components/Visualization.jsx';
 import Prototype from './components/Prototype.jsx';
 import Development from './components/Development.jsx';
-import { Switch, Route } from 'react-router';
+import { Router, Switch, Route } from 'react-router';
 
 import './scss/main.scss';
 
 const browserHistory = createBrowserHistory();
 
 render(
-  <Switch history={browserHistory}>
-    <Route exact path='/' component={HomePage}/>
-    <Route path='/visualizations' component={Visualization}/>
-    <Route path='/prototypes' component={Prototype}/>
-    <Route path='/development' component={Development}/>
-  </Switch>,
+  <Router history={browserHistory}>
+    <Switch>
+      <Navbar/>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/visualizations' component={Visualization}/>
+      <Route path='/prototypes' component={Prototype}/>
+      <Route path='/development' component={Development}/>
+    </Switch>
+  </Router>,
   document.getElementById('react-src')
 );
