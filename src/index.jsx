1b128c8a4fa85ad506082b1353fddbbe3218ca9a
kcirekcom <erick.f.mock@gmail.com>
2018-10-23 22:03:16 -0700

    Restructured index.jsx file to only wrap around CustomizeBackground component CustomizeBackground component is the main wrapper that now houses all other components Moved AsyncComponent logic over to background component

--- src/index.jsx
+++ src/index.jsx
@@ -2,51 +2,15 @@
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Loadable from 'react-loadable';
-import Loader from './components/Loader.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
 import CustomizeBackground from './components/CustomizeBackground.jsx';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
-import ScrollToTop from './components/ScrollToTop.jsx';
 import './scss/main.scss';
-
-const AsyncHome = Loadable({
-  loader: () => import('./components/Home.jsx'),
-  loading: Loader
-});
-
-const AsyncVisualization = Loadable({
-  loader: () => import('./components/Visualization.jsx'),
-  loading: Loader
-});
-
-const AsyncPrototype = Loadable({
-  loader: () => import('./components/Prototype.jsx'),
-  loading: Loader
-});
-
-const AsyncDevelopment = Loadable({
-  loader: () => import('./components/Development.jsx'),
-  loading: Loader
-});
 
 class App extends Component {
   render() {
     return(
       <Router>
-        <CustomizeBackground>
-          <Navbar/>
-          <ScrollToTop>
-            <Switch>
-              <Route exact path='/' component={AsyncHome}/>
-              <Route exact path='/visualizations' component={AsyncVisualization}/>
-              <Route exact path='/prototypes' component={AsyncPrototype}/>
-              <Route exact path='/development' component={AsyncDevelopment}/>
-            </Switch>
-          </ScrollToTop>
-          <Footer/>
-        </CustomizeBackground>
+        <CustomizeBackground/>
       </Router>
     )
   }
