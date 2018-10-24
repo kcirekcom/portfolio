339f8ede3033a9c4683cd257f6cb27cedf506496
kcirekcom <erick.f.mock@gmail.com>
2018-10-23 21:58:58 -0700

    Completed logic for the CustomizeBackground component which is visible throughout site Set up styles for the background component and the background widget that displays the background choices Transition style for min width of 650px has to be cleaned up but good for now

--- src/components/CustomizeBackground.jsx
+++ src/components/CustomizeBackground.jsx
@@ -1,36 +1,95 @@
 'use strict';
 
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
+import Loader from './Loader.jsx';
+import Navbar from './Navbar.jsx';
+import Footer from './Footer.jsx';
+import ScrollToTop from './ScrollToTop.jsx';
+
+const AsyncHome = Loadable({
+  loader: () => import('./Home.jsx'),
+  loading: Loader
+});
+
+const AsyncVisualization = Loadable({
+  loader: () => import('./Visualization.jsx'),
+  loading: Loader
+});
+
+const AsyncPrototype = Loadable({
+  loader: () => import('./Prototype.jsx'),
+  loading: Loader
+});
+
+const AsyncDevelopment = Loadable({
+  loader: () => import('./Development.jsx'),
+  loading: Loader
+});
 
 export default class CustomizeBackground extends Component {
   constructor() {
     super();
-    this.background = null
+    this.state = {
+      background: 'background--first',
+      hidden: true
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ background: localStorage.getItem('background') });
+  }
+
+  showWidget = (event) => {
+    if (event.key == 'Enter' || event.type == 'click') {
+      this.setState({ hidden: !this.state.hidden })
+    }
   }
 
   selectBackground = (event) => {
     event.persist();
-
-    switch (input) {
-      case 'second':
-        this.setState({});
-        break;
-      case 'third':
-        this.setState({});
-        break;
-      default:
-        this.setState({});
+    let value = event.target.attributes.value.value;
+    if (event.key == 'Enter' || event.type == 'click') {
+      switch (value) {
+        case 'second':
+          localStorage.setItem('background', 'background--second');
+          this.setState({ background: 'background--second' });
+          break;
+        case 'third':
+          localStorage.setItem('background', 'background--third');
+          this.setState({ background: 'background--third' });
+          break;
+        default:
+          localStorage.setItem('background', 'background--first');
+          this.setState({ background: 'background--first' });
+      }
     }
   }
 
   render() {
     return(
-      <div className={`background ${this.background}`}>
-        <div className='background-selector'>
-          <span value='first' onClick={this.selectBackground}/>
-          <span value='second' onClick={this.selectBackground}/>
-          <span value='third' onClick={this.selectBackground}/>
+      <div className={`background ${this.state.background}`} aria-hidden='true'>
+        <Navbar {...this.state}/>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path='/' component={AsyncHome}/>
+            <Route exact path='/visualizations' component={AsyncVisualization}/>
+            <Route exact path='/prototypes' component={AsyncPrototype}/>
+            <Route exact path='/development' component={AsyncDevelopment}/>
+          </Switch>
+        </ScrollToTop>
+
+        <div className='background-widget'>
+          <div className={`background-widget__label ${this.state.hidden === false ? 'background-widget__label--expanded': ''}`} onClick={this.showWidget} onKeyUp={this.showWidget} tabIndex='0'><div className='icon-stack'/></div>
+          <div className={`background-widget__selector ${this.state.hidden === false ? 'background-widget__selector--visible': ''}`}>
+            <span value='first' onClick={this.selectBackground} onKeyUp={this.selectBackground} tabIndex='0'/>
+            <span value='second' onClick={this.selectBackground} onKeyUp={this.selectBackground} tabIndex='0'/>
+            <span value='third' onClick={this.selectBackground} onKeyUp={this.selectBackground} tabIndex='0'/>
+          </div>
         </div>
+
+        <Footer/>
       </div>
     )
   }
