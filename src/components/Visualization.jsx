c4ffe1dc847a21242e0cb94433db1d28c8f015a0
kcirekcom <erick.f.mock@gmail.com>
2020-01-29 20:44:24 -0800

    Remove video/after effects visualizations

--- src/components/Visualization.jsx
+++ src/components/Visualization.jsx
@@ -34,16 +34,6 @@ export default class Visualization extends Component {
             <a className='visual__link' href='https://mir-cdn.behance.net/v1/rendition/project_modules/1400/d0463353458781.59359572bc6f2.jpg' target='_blank'>Bēhance Link</a>
             <img className='visual__img' src={require('../assets/data-viz/Pioneering-Psychology.jpg')} alt='A visualization about the origins of psychology, which groups key contributors into their respective disciplines and schools of thought. The disciplines observed include functionalism, structuralism, behaviorism, cognitivism, humanistic/gestalt, psychoanalytic/psychosocial, and neuropsychology.'/>
           </div>
-
-          <h2>Star Facts</h2>
-          <div className='visual'>
-            <iframe className='visual__iframe' src='https://www.youtube.com/embed/wpE_EevJBmg' frameBorder='0' allowFullScreen></iframe>
-          </div>
-
-          <h2>Glacier Facts</h2>
-          <div className='visual'>
-            <iframe className='visual__iframe' src='https://www.youtube.com/embed/V3voeljivNs' frameBorder='0' allowFullScreen></iframe>
-          </div>
         </div>
       </main>
     )
